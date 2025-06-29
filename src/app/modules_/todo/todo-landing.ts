fee7ca785095f9f316588de4767003b6a33aeab0
yeganehlorigoeeni <yeganehgoeeni.8@gmail.com>
2025-06-30 02:21:11 +0330

    ADD:The Routes and the Module

---
+++ src/app/modules_/todo/todo-landing.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+
+@Component({
+  selector: 'app-todo-landing',
+  imports: [RouterOutlet],
+  templateUrl: './todo-landing.html',
+  styleUrl: './todo-landing.css'
+})
+export class TodoLanding {
+
+}
